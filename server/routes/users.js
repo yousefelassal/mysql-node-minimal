9cb0da8c0e8ea971f441579a27eaace7da631140
Yousef <yousefsameh24@gmail.com>
2024-03-20 16:50:10 +0200

    Add database query to retrieve customers

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db.query('SELECT * FROM customers', (err, rows) => {
+    if (err) {
+      res.status(500).send('Error');
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 module.exports = router;
